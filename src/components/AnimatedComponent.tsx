fc82042b9ddc3f8c8d044c85c293f24c1b3da7cd
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-11-28 07:16:03 +0000

    feat: Added reset button and delay to clearly identify animation start point

--- src/components/AnimatedComponent.tsx
+++ src/components/AnimatedComponent.tsx
@@ -14,45 +14,83 @@ import { useState } from "react";
  */
 const AnimatedComponent = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isReset, setIsReset] = useState(true);
 
   // Get the filter values and functions from zustand
   const filters = useFilters();
 
   /**
-   * Handles the click event of the play button.
-   * Sets the isPlaying state to true to disable the play button,
-   * generates the from and to objects based on the filters,
-   * converts the duration from seconds to milliseconds,
-   * plays the animation using gsap.fromTo,
-   * and sets the isPlaying state to false after the animation is complete
-   * so the user can play the animation again.
+   * Plays the animation by getting the to object from the filters
+   * and converting the duration from seconds to milliseconds.
+   * Then, it plays the animation using GSAP's to method.
+   * Once the animation is complete, it sets the isPlaying state to
+   * false so the user can play the animation again.
+   * @returns {void}
    */
-  const onClickHandler = () => {
-    // Set the isPlaying state to true to disable the play button
-    setIsPlaying(true);
-
-    // Get the from and to objects
-    const gsapFrom = generateGSAPFromObject();
+  const playAnimation = () => {
+    // Get the to object
     const gsapTo = generateGSAPToObject(filters);
 
     // Convert the duration from seconds to milliseconds
     const durationMs = filters.duration * 1000;
 
     // Play the animation
-    gsap.fromTo(".box", gsapFrom, gsapTo);
+    gsap.to(".box", gsapTo);
 
     // Once the animation is complete, set the isPlaying state to
     // false so the user can play the animation again
     setTimeout(() => {
+      setIsReset(false);
       setIsPlaying(false);
     }, durationMs);
   };
 
+  /**
+   * Handles the play button click event by setting the isPlaying state to true
+   * and checking if the animation is already reset. If the animation is already
+   * reset, it plays the animation immediately. If not, it resets the animation
+   * before playing and waits for 0.5 seconds before playing the animation so the
+   * user can clearly see where the animation starts from.
+   */
+  const onPlayHandler = () => {
+    // Set the isPlaying state to true to disable the play button
+    setIsPlaying(true);
+
+    // If the animation is already reset, play the animation immediately
+    if (isReset) playAnimation();
+    else {
+      // Reset the animation before playing
+      onResetHandler();
+
+      // Wait for 0.5 seconds before playing the animation so the user
+      // can clearly see where the animation starts from
+      setTimeout(() => {
+        playAnimation();
+      }, 500);
+    }
+  };
+
+  /**
+   * Resets the animation before playing by setting the from object.
+   * It sets the isReset state to true after resetting the animation.
+   */
+  const onResetHandler = () => {
+    // Reset the animation before playing by setting the from object
+    const gsapFrom = generateGSAPFromObject();
+    gsap.set(".box", gsapFrom);
+    setIsReset(true);
+  };
+
   return (
     <section className="flex-col animation-section">
-      <button onClick={onClickHandler} disabled={isPlaying}>
-        Play
-      </button>
+      <div className="flex-row">
+        <button onClick={onPlayHandler} disabled={isPlaying}>
+          Play
+        </button>
+        <button onClick={onResetHandler} disabled={isPlaying}>
+          Reset
+        </button>
+      </div>
       <div className="animation-area">
         <div className="box"></div>
         <ul className="animation-area-x-axis">
